646e72207cef46b2b5b238814924336bac259f3f
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-13 12:24:09 +0200

    Make home view, route and controller leading to home, make layout-file

---
+++ laravel-and-beyond/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Beer Buddy</title>
+</head>
+<body>
+    <h1>Beer Buddy</h1>
+    <h2>"Discover, connect, and elevate your beer experience"</h2>
+    
+    <p>Beer Buddy is a fantastic app designed to enhance your beer-drinking experience. Whether you're a beer enthusiast or a casual drinker, this app has everything you need to discover new brews, learn about beer styles, and connect with other beer lovers.
+        With Beer Buddy, you can easily search for beers by name, style, or brewery and get detailed information about each one, including ABV, IBU, and tasting notes. You can also keep track of your favorite beers and create a personalized beer journal to record your tasting experiences.
+        One of the best features of Beer Buddy is its social aspect. You can connect with other beer lovers in the app and share your thoughts on different beers. You can also join groups, participate in beer-related discussions, and discover new breweries and events near you.
+        Overall, Beer Buddy is an essential tool for anyone who loves beer and wants to expand their knowledge and appreciation of this fantastic beverage.</p>
+    
+    <h3>"Get your beer journey started with Beer Buddy - your ultimate beer companion!"</h3>
+</body>
+</html>
