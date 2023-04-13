1aec3075aa453760db8ec77d2975699131c249d4
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-13 15:50:24 +0200

    Include navbar and footer

--- laravel-and-beyond/resources/views/includes/footer.blade.php
+++ laravel-and-beyond/resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
-<div class="row">
+<div class="row fixed-bottom">
     <div class="col"><hr></div>
-    <div class="col-auto text-muted text-uppercase">© Copyright 2023 BeCode </div>
+    <div class="col-auto text-muted">© Copyright 2023 • BeerBuddy.com </div>
     <div class="col"><hr></div>
 </div>
 
--- laravel-and-beyond/resources/views/layouts/layout.blade.php
+++ laravel-and-beyond/resources/views/layouts/layout.blade.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@%5E2.2.7/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Beer Buddy</title>
 </head>
 <body>
