2894e79f110757972a8d39c3d6b6f17f27e672ba
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-18 10:15:24 +0200

    Continue on styling beer cards

--- laravel-and-beyond/resources/views/beers.blade.php
+++ laravel-and-beyond/resources/views/beers.blade.php
@@ -2,10 +2,10 @@
 @section('content')
 
 <h1 class="text-center mx-auto mt-3 mb-5">Beers</h1>
-<div class="container d-flex flex-wrap mx-auto w-100" id="card-container">
+<div class="container d-flex flex-wrap mx-auto w-100 gap-1" id="card-container">
 
     @foreach ($beers as $beer)
-        <p class="card-text mb-3 mx-3" style="max-width: 300px;">< x-beer-preview-card :beer="$beer" /></p>
+        <x-beer-preview-card :beer="$beer"/>
     @endforeach
 </div>
 
--- laravel-and-beyond/resources/views/components/beer-preview-card.blade.php
+++ laravel-and-beyond/resources/views/components/beer-preview-card.blade.php
@@ -1,17 +1,23 @@
 @props(['beer'])
 
-<div class="card mb-5 mr-4 mx-auto" style="max-width: 450px; max-width: 370px;">
+<div class="card mb-5 mx-auto" style="max-width: 450px; max-width: 370px;">
     <img class="card-img-top" src="{{$beer->image}}" alt="Image of the displayed beer">
-    <div class="card-body">
-      <h5 class="card-title">{{$beer->name}}</h5>
+    <div class="card-body mx-4">
+      <h3 class="card-title">{{$beer->name}}</h3>
       <p class="card-text">{{$beer->type}}</p>
       <p class="card-text">{{$beer->country}}</p>
       <p class="card-text">{{$beer->alcohol_percentage}}%</p>
       <p class="card-text">BREWERY: {{$beer->brewery}}</p>
       
-      <a href="/beers/{{$beer->slug}}" class="btn btn-primary">More info</a>
-      <a href="#" class="btn btn-primary">Edit</a>
-      <a href="#" class="btn btn-primary">Delete</a>
+      <div class="d-flex justify-content-center align-items-center mt-4">
+        <div class=" flex-column h-15 w-40">
+        
+          <a href="/beers/{{$beer->slug}}" class="btn btn-outline-dark btn-block shadow">Info</a>
+          <a href="#" class="btn btn-outline-dark btn-block shadow">Edit</a>
+          <a href="#" class="btn btn-outline-dark btn-block shadow">Delete</a>
+        </div>
+      </div>
+      
 
     </div>
   </div>
