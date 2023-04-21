75ce0c8c70485b3c8bf4107fbea2f70d62615b2c
Sofie Van Den Abeele <sofievandenabeele@MacBook-Pro-van-Sofie.local>
2023-04-21 17:01:07 +0200

    Add styling to Profile blade

--- laravel-and-beyond/resources/views/user/profile.blade.php
+++ laravel-and-beyond/resources/views/user/profile.blade.php
@@ -2,6 +2,10 @@
 @section('content')
 <div class="container">
 <h1>Hello {{ Auth::user()->name }}</h1>
-<h2>You have successfully logged in!</h2>
+<h2>You are logged in!</h2>
+<div class="profile-links">
+    <a class="profile-link" href="{{route('create')}}"><img src="{{asset("./Images/beerpump.png")}}" alt="beer pump"></a>
+    <a class="profile-link" href="{{route('new-beers')}}"><img src="{{asset("./Images/woodenbeer.png")}}" alt="beer barrel"></a>
+</div>
 </div>
 @endsection
